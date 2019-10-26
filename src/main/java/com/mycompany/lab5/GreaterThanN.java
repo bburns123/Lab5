60d8818f35c01c343fa022b183f79e2efc0469b1
Brendyn Burns <Brendyn Burns@DESKTOP-9USEOT4>
2019-10-26 11:56:45 -0500

    GreaterThanN first commit

---
+++ src/main/java/com/mycompany/lab5/GreaterThanN.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab5;
+
+/**
+ *
+ * @author Brendyn Burns
+ * Chapter 7, question 5.
+ */
+public class GreaterThanN {
+    public static void arrayMethod(int array[], int n) {
+        
+        for (int counter = 1; counter < array.length; counter++) {
+            if (array[counter] > n) {
+                System.out.println(array[counter] + " is greater than " + n);
+            }
+        }
+    }
+}
